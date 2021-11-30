6c1f6939bb92b2bc9d8414f08cb37f9493aa8747
Alex Luce <alexanderluce@gmail.com>
2021-11-30 10:02:39 -0500

    u

--- src/Convert.js
+++ src/Convert.js
@@ -12,6 +12,21 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { TransferRequest, createZeroConnection, createZeroUser } from 'zero-protocol/dist/lib/zero.js';
+
+/*
+DEVELOPMENT CONSTANTS
+*/
+
+const connectedWallet = '0xD903338baE3D5C59259E562a49E4ab177E3149a1';
+const zeroModule = '0x59741D0210Dd24FFfDBa2eEEc9E130A016B8eb3F'; // arbitrum convert module address
+const trivialUnderwriter = '0xd0D8fA764352e33F40c66C75B3BC0204DC95973e';
+const asset = '0xDBf31dF14B66535aF65AaC99C32e9eA844e14501'; // renBTC on arbitrum
+const data = '0x0';
+
+
+
+
 
 function Copyright(props) {
   return (
@@ -28,16 +43,40 @@ function Copyright(props) {
 
 const theme = createTheme();
 
-export default function SignIn() {
+export default function Submit() {
+
+  const [user, setUser] = React.useState(null);
+
+  const initializeConnection = async () => {
+    const connection = await createZeroConnection('/dns4/lourdehaufen.dynv6.net/tcp/443/wss/p2p-webrtc-star/');
+    const user = createZeroUser(connection);
+    await user.conn.start();
+    await user.subscribeKeepers();
+    setUser(user);
+  }
+
+  initializeConnection();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
+
+    const transferRequest = new TransferRequest({
+      to: connectedWallet,
+      underwriter: trivialUnderwriter,
+      module: zeroModule,
+      asset,
+      amount,
+      data
+    })
+
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      amount: data.get('amount'),
+      keepers: user.keepers
     });
   };
+
+  const [address, setAddress] = React.useState(null);
 
   return (
     <ThemeProvider theme={theme}>
@@ -58,23 +97,26 @@ export default function SignIn() {
               margin="normal"
               required
               fullWidth
-              id="email"
+              id="amount"
               label="BTC Amount"
-              name="email"
+              name="amount"
 	      color="success"
-              autoComplete="email"
               autoFocus
             />
             <Button
               type="submit"
               fullWidth
-	      color="success"
+	            color="success"
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
               Move
             </Button>
           </Box>
+          {!address && `Deposit Address: ${address}`}
+        </Box>
+        <Box>
+        {`Status: ${user ? "Connected" : "Disconnected"}`}
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
--- src/Orders.js
+++ src/Orders.js
@@ -9,11 +9,15 @@ import Title from './Title';
 import { ethers } from 'ethers';
 import moment from 'moment';
 
+const address = '0xD903338baE3D5C59259E562a49E4ab177E3149a1'
+
 const rows = ['0.001', '0.002', '0.003'].map((v) => ({
   amount: v,
   underwriter: ethers.Wallet.createRandom().address,
   arbiscan: ethers.utils.hexlify(ethers.utils.randomBytes(32)),
-  date: moment(new Date()).format('MM-DD-YYYY HH:mm')
+  date: moment(new Date()).format('MM-DD-YYYY HH:mm'),
+  actualRen: v * 0.95,
+  actualArbEth: v * 0.05
 }));
 
 function preventDefault(event) {
@@ -30,6 +34,9 @@ export default function Orders() {
             <TableCell>Date</TableCell>
             <TableCell>Underwriter</TableCell>
             <TableCell>Amount</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>renBTC</TableCell>
+            <TableCell>arbETH</TableCell>
             <TableCell>Arbiscan</TableCell>
           </TableRow>
         </TableHead>
@@ -39,6 +46,9 @@ export default function Orders() {
               <TableCell>{row.date}</TableCell>
               <TableCell>{row.underwriter}</TableCell>
               <TableCell>{row.amount}</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>{row.actualRen.toFixed(5)}</TableCell>
+              <TableCell>{row.actualArbEth.toFixed(5)}</TableCell>
               <TableCell><a style={ { color: 'black', textDecoration: 'none' } } href={'https://arbiscan.io/tx/' + row.arbiscan}>{ row.arbiscan }</a></TableCell>
             </TableRow>
           ))}
