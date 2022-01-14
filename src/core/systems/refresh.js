4cf605d02e41fe360a609370340462a0daefbc7e
oobiora <oo608@nyu.edu>
2022-01-14 09:14:00 -0500

    olisa/close-to-full-prod

--- src/core/systems/refresh.js
+++ src/core/systems/refresh.js
@@ -15,21 +15,6 @@ export function useLocalStorageRefresh(props){
     }, [])
 }
 
-<<<<<<< HEAD
-=======
-    useEffect(() => {
-        if (wrapper){
-            const value = wrapper
-                .chain()
-                .head()
-                .value()
-
-            _BridgeMonitor.listener.emit("background", value.value)
-            _BridgeMonitor.listener.on("cleared", () => {
-                value.value.status = "success"
-                window.localStorage.setItem(value.key, JSON.stringify(value.value))
-            })
->>>>>>> fe4d9728b215a90d7cb0a327badfde00cb09cd92
 
 
 /**
