515012ae5f8ecd181d65b3f2e2359e34c3265280
yoyobojo <angellolazar@gmail.com>
2022-03-06 22:12:50 -0500

    yoyobojo - progress on test suite

--- src/__tests__/bridge.test.js
+++ src/__tests__/bridge.test.js
@@ -1,12 +1,58 @@
 import renderer from 'react-test-renderer';
 import { BridgeComponent } from '../components/organisms/bridge';
+import { ConvertInput } from '../components/atoms/inputs';
+import { Wrappers } from '../core/systems/bridge';
+import { library } from '../components/utils/tokens';
+import { ethers } from 'ethers';
 
 describe("Bridge", () => {
     it('renders correctly', () => {
-        const bridge = renderer
-          .create(<BridgeComponent />)
-          .toJSON();
+      const bridge = renderer.create(
+        <BridgeComponent />
+      ).toJSON();
           
         expect(bridge).toMatchSnapshot();
     });
+
+    it('accepts user input', () => {
+      const onConvertMock = jest.fn();
+      const event = {
+        preventDefault() {},
+        target: { value: '100' }
+      };
+
+      const convertInput = renderer.create(
+        <ConvertInput 
+          value={event.target.value}
+          onChange={onConvertMock}
+        />
+      ).toJSON();
+
+      expect(convertInput.children[0].props.value).toBe('100');
+    })
+    it('converts user input', async () => {
+      // TODO - define global.wallet's "connection" and "connectWallet"
+      const inputVal = '1';
+      const token = library['bitcoin'];
+      const btcPrice = ethers.utils.formatUnits((await token.priceFeed).toString(), 6);
+      const usdFormatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      });
+      usdFormatter.format(inputVal * btcPrice);
+    })
+
+    it('completes a transfer', () => {
+    //   const { result } = renderHook(() => useTransactionSender());
+
+    //   const mechanism = renderer.create(
+    //     <BridgeProvider>
+    //       <TransferTool
+    //         _isLoading={false}
+    //         _action={result.current.sign}
+    //         _context={Provider} 
+    //       />
+    //     </BridgeProvider>
+    //   )
+    })
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,8 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-// import '@testing-library/jest-dom';
-
+import '@testing-library/react-hooks';
 import React from 'react'
 import { TextEncoder, TextDecoder } from 'util'
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 global.React = React;
+global.wallet = { connection, connectWallet };
+const { connection, connectWallet } = global.wallet;
