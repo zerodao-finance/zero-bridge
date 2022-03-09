26ad84389d3f26add779fd12ba31c03edeec204a
olisa obiora <olisaobiora@pop-os.localdomain>
2022-03-08 20:35:45 -0500

    olisa/progress_check

--- src/api/global/global.reducers.js
+++ src/api/global/global.reducers.js
@@ -108,46 +108,4 @@ export const globalBridgeReducer = (state, action) => {
             assertNever(action.type)
     }
 
-    
-    // switch (action.type) {
-    //     case "UPDATE_INPUT":
-    //         switch (action.payload.type) {
-    //             case "RATIO":
-    //                 return { ...state, input: { ...state.input, ratio: action.payload.data}}
-    //             case "AMOUNT":
-    //                 return { ...state, input: { ...state.input, amount: action.payload.data}}
-    //             case "RESET":
-    //                 return { ...state, input: { ratio: 0, amount: 0} }
-    //             default:
-    //                 assertNever(action.payload.type)
-    //         }
-    //         break
-        
-    //     case "START_DISPLAY_REQUEST":
-    //         return { ...state, isLoading: true, error: null}
-
-    //     case "STATE_CONNECTION_REQUEST":
-    //         return { ...state, isLoading: true, error: null}
-    //     case "FAIL_CONNECTION_REQUEST":
-    //         return { ...state, isLoading: false, error: new Error("Could not establish Wallet Connection"), wallet: null, network: null}
-    //     case "SUCCEED_CONNECTION_REQUEST":
-    //         return { ...state, isLoading: false, error: null, wallet: action.payload.data.wallet, network: action.payload.data.network, address: action.payload.data.address, network_config: action.payload.data.network_config}
-    //     case "SUCCEED_DISPLAY_REQUEST":
-    //         return { ...state, isLoading: false, error: null, display: action.payload.data}
-    //     case "FAIL_DISPLAY_REQUEST":
-    //         return { ...state, isLoading: false, error: new Error("Could Not Update Values"), display: { ETH: 0, renBTC: 0}}
-    //     case "GET_TRANSACTION":
-    //         return { ...state, transactions: action.payload.data}
-    //     case "ADD_TRANSACTION":
-    //         switch (action.payload.type) {
-    //             case "RELEASE":
-    //                 return { ...state, transactions: {...state.transactions, release: [action.payload.data, ...state.transactions.release]}}
-    //             case "TRANSFER":
-    //                 return { ...state, transactions: {...state.transactions, transfer: [action.payload.data, ...state.transactions.transfer]}}
-    //             default:
-    //                 assertNever(action.payload.type)
-    //         }
-    //     default:
-    //         assertNever(action.type)
-    // }
 }
