1f8ceb83d1c77675eae1f3011769bedc5be85847
Enzo Yono <nicholas.khami@gmail.com>
2022-03-10 13:55:19 -0600

    increase width to accomodate full values

--- src/ui/molecules/bridge.transfer/bridge.transfer.result.js
+++ src/ui/molecules/bridge.transfer/bridge.transfer.result.js
@@ -10,7 +10,7 @@ export const BridgeTransferResult = ({ETH, renBTC}) => {
 
         <div className="w-fit grid grid-cols-2 gap-3 self-center">
             <div className="text-emerald-500 border-r-[1px] border-emerald-700 dark:border-slate-500 rounded-md px-4 py-2 flex flex-col justify-center items-center divide-y max-w-[90px] truncate w-[90px]">
-                <p className='max-w-[50px] truncate'>
+                <p className='max-w-[75px] truncate'>
                     {ETH}
                 </p>
                 <p className="text-[13px]">
@@ -18,7 +18,7 @@ export const BridgeTransferResult = ({ETH, renBTC}) => {
                 </p>
             </div>
             <div className="text-emerald-500 border-l-[1px] border-emerald-700 dark:border-slate-500 rounded-md px-4 py-2 flex flex-col justify-center items-center divide-y max-w-[90px] truncate w-[90px]">
-                <p className="max-w-[50px] truncate">
+                <p className="max-w-[75px] truncate">
                     {renBTC}
                 </p>
                 <p className="text-[13px]">
