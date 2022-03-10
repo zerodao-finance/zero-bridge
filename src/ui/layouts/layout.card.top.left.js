5f15d134fe7803cbeb63c8b91cabe41d37f5400a
olisa obiora <olisaobiora@pop-os.localdomain>
2022-03-09 19:02:09 -0500

    olisa/progress/check

---
+++ src/ui/layouts/layout.card.top.left.js
@@ -0,0 +1,9 @@
+export const TopLeftCardLayout = ({ children }) => {
+    return (
+        <div className=" dark:text-white isolate relative z-[30]">
+        {
+            children
+        }
+        </div>
+    )
+}
