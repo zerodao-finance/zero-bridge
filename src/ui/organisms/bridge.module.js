700ed1801d1e37aae80e7aeb052e2d33cd1e38e3
mitche50 <mitche50@gmail.com>
2022-03-28 11:30:26 -0400

    chore: update typography and logos

--- src/ui/organisms/bridge.module.js
+++ src/ui/organisms/bridge.module.js
@@ -8,8 +8,8 @@ export const BridgeModule = () => {
     const { toggleMode, back, mode, processing, signed, data } = useBridgePage()
     return (
         <div className='flex flex-col container h-fit bg-white shadow-xl rounded-[30px] justify-center place-items-center gap-1 md:gap-3  first:gap-0 w-fit pb-4 dark:bg-gray-700 text-white min-w-[370px]'>
-            <p className="text-lg font-light text-black tracking-wider w-full bg-emerald-300 text-center shadow-md rounded-t-md">Bridge Funds</p>
-            <div className={`h-full w-full grid grid-cols-2 grid-flow-rows mb-8 bg-gray-200 dark:bg-gray-800 pt-3 align-center font-light tracking-wider text-sm text-center`}>
+            <p className="text-lg font-header text-black tracking-wider w-full bg-emerald-300 text-center shadow-md rounded-t-md">Bridge Funds</p>
+            <div className={`h-full w-full grid grid-cols-2 grid-flow-rows mb-8 bg-gray-200 dark:bg-gray-800 align-center font-light tracking-wider text-sm text-center`}>
             </div>
             { processing ? 
             
