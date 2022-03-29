64322a4378ad0c94ae84b4849bf311d8325fa258
mitche50 <mitche50@gmail.com>
2022-03-28 23:18:17 -0400

    chore: make sidebar buttons more clickable

--- src/ui/molecules/navigation/navigation.sidebar.default.js
+++ src/ui/molecules/navigation/navigation.sidebar.default.js
@@ -11,8 +11,8 @@ export const  DefaultNavigationSidebar = ({changeModule}) => {
     return (
         <div className="min-h-full flex fixed right-0 bg-white w-20 hover:w-[14rem] hover:rounded-l-[7rem] group rounded-l-[7rem] shadow-2xl z-50 dark:bg-emerald-300 transition-all ease-in-out duration-150 text-black font-light text-sm">
             <div className="grow w-full flex flex-col items-center justify-between pb-40 mt-[8rem]">
-                <div className="flex flex-col gap-8 w-full">
-                    <div className={"flex flex-row gap-3 w-full py-2 hover:underline decoration-2 decoration-emerald-300 dark:decoration-white cursor-pointer hover:bg-slate-100 dark:hover:bg-emerald-400 "} onClick={action}>
+                <div className="flex flex-col gap-4 w-full">
+                    <div className={"flex flex-row gap-1 w-full py-4 hover:underline decoration-2 decoration-emerald-300 dark:decoration-white cursor-pointer hover:bg-slate-100 dark:hover:bg-emerald-400 "} onClick={action}>
                         <div className="flex flex-row mx-auto">
                             <RiExchangeFundsLine className="h-[1.2rem] w-[1.2rem] dark:fill-black"/>
                             <div className="hidden group-hover:block opacity-0 group-hover:opacity-100 pl-3">
@@ -20,7 +20,7 @@ export const  DefaultNavigationSidebar = ({changeModule}) => {
                             </div>
                         </div>
                     </div>
-                    <div className={"flex flex-row gap-3 w-full py-2 hover:underline decoration-2 decoration-emerald-300 dark:decoration-white cursor-pointer hover:bg-slate-100 dark:hover:bg-emerald-400 "} onClick={action}>
+                    <div className={"flex flex-row gap-3 w-full py-4 hover:underline decoration-2 decoration-emerald-300 dark:decoration-white cursor-pointer hover:bg-slate-100 dark:hover:bg-emerald-400 "} onClick={action}>
                         <div className="flex flex-row mx-auto">
                             <MdOutlinePending className="h-[1.2rem] w-[1.2rem] dark:fill-black"/>
                             <div className="hidden group-hover:block opacity-0 group-hover:opacity-100 pl-3">
@@ -28,7 +28,7 @@ export const  DefaultNavigationSidebar = ({changeModule}) => {
                             </div>
                         </div>
                     </div>
-                    <div className={"flex flex-row gap-3 w-full py-2 hover:underline decoration-2 decoration-emerald-300 dark:decoration-white cursor-pointer hover:bg-slate-100 dark:hover:bg-emerald-400 "} onClick={action}>
+                    <div className={"flex flex-row gap-3 w-full py-4 hover:underline decoration-2 decoration-emerald-300 dark:decoration-white cursor-pointer hover:bg-slate-100 dark:hover:bg-emerald-400 "} onClick={action}>
                         <div className="flex flex-row mx-auto">
                             <BiTransfer className="h-[1.2rem] w-[1.2rem] dark:fill-black"/>
                             <div className="hidden group-hover:block opacity-0 group-hover:opacity-100 pl-3">
@@ -36,7 +36,7 @@ export const  DefaultNavigationSidebar = ({changeModule}) => {
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-row gap-3 w-full py-2 hover:underline decoration-2 decoration-emerald-300 dark:decoration-white cursor-pointer hover:bg-slate-100 dark:hover:bg-emerald-400" onClick={() => window.open("https://docs.zerodao.com")}>
+                    <div className="flex flex-row gap-3 w-full py-4 hover:underline decoration-2 decoration-emerald-300 dark:decoration-white cursor-pointer hover:bg-slate-100 dark:hover:bg-emerald-400" onClick={() => window.open("https://docs.zerodao.com")}>
                         <div className="flex flex-row mx-auto">
                             <RiFileListLine className="h-[1.2rem] w-[1.2rem] dark:fill-black"/>
                             <div className="hidden group-hover:block opacity-0 group-hover:opacity-100 pl-3">
