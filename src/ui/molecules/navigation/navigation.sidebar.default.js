3f63bf33ab906a0f2d03e8595e36555aa9436d1c
Nick K <nicholas.khami@gmail.com>
2022-04-05 13:50:49 -0500

    skin finished

--- src/ui/molecules/navigation/navigation.sidebar.default.js
+++ src/ui/molecules/navigation/navigation.sidebar.default.js
@@ -10,7 +10,7 @@ export const  DefaultNavigationSidebar = ({changeModule}) => {
 
     }
     return (
-        <div className="min-h-full hidden md:block flex fixed right-0 bg-white w-20 hover:w-[14rem] group rounded-l-[2rem] shadow-2xl z-50 dark:bg-badger-yellow-500 transition-all ease-in-out duration-150 text-black font-light text-sm">
+        <div className="min-h-full hidden md:block flex fixed right-0 bg-white w-20 hover:w-[14rem] group rounded-l-[2rem] shadow-2xl z-50 dark:bg-badger-yellow-500 transition-all ease-in-out duration-150 text-black font-bold text-sm">
             <div className="grow w-full flex flex-col items-center justify-between pb-40 mt-[8rem]">
                 <div className="flex flex-col gap-4 w-full">
                     <div className={"flex flex-row gap-1 w-full py-4 hover:underline decoration-2 decoration-main-green dark:decoration-badger-blue-400  cursor-pointer hover:bg-slate-100 dark:hover:bg-black/10 transition ease-in-out duration-150 "} onClick={action}>
