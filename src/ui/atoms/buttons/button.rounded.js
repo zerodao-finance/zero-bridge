37c490ec6a8ca0688ca321f6b9dd783e7790481f
Nick K <nicholas.khami@gmail.com>
2022-04-05 15:28:02 -0500

    added captured button

--- src/ui/atoms/buttons/button.rounded.js
+++ src/ui/atoms/buttons/button.rounded.js
@@ -1,9 +1,9 @@
 export const PrimaryRoundedButton = ({ active, label, action }) => {
 	const getClass = () => {
 		if (active) {
-			return 'transition ease-in-out duration-150 px-2 py-1 hover:bg-badger-yellow-400/10 active:bg-badger-yellow-400/40 font-bold w-full rounded-xl border border-badger-yellow-400 text-badger-yellow-400 w-full truncate text-[13px] md:text-[15px]';
+			return 'transition ease-in-out duration-150 px-2 py-1 hover:bg-badger-yellow-400/80 active:bg-badger-yellow-400/40 font-bold w-full rounded-md bg-badger-yellow-400 text-badger-black-800 w-full truncate text-[13px] md:text-[15px]';
 		}
-		return 'transition ease-in-out duration-150 px-2 py-1 font-bold w-full rounded-xl border border-badger-yellow-400/10 text-badger-yellow-400/10 w-full truncate text-[13px] md:text-[15px]'; 
+		return 'transition ease-in-out duration-150 px-2 py-1 font-bold w-full rounded-md text-badger-gray-200 bg-badger-gray-400 w-full truncate text-[13px] md:text-[15px]'; 
 	};
 	return (
 		<>
