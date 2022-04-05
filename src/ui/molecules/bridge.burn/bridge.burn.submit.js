1ccd926bb65c26be86bbdcd6bcaabafb0cbfa716
Nick K <nicholas.khami@gmail.com>
2022-04-05 12:03:19 -0500

    change colors for primary button

--- src/ui/molecules/bridge.burn/bridge.burn.submit.js
+++ src/ui/molecules/bridge.burn/bridge.burn.submit.js
@@ -1,5 +1,5 @@
 import { PrimaryRoundedButton } from '../../atoms/buttons/button.rounded';
 
 export const BridgeBurnSubmit = ({ action }) => {
-	return <PrimaryRoundedButton active={true} label={'Release Funds'} action={action} />;
+	return <PrimaryRoundedButton active={false} label={'Release Funds'} action={action} />;
 };
