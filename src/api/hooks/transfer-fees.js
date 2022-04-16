fb2ed7d2a2b99a06a821e5346ad5deeb2616f841
Nick K <nicholas.khami@gmail.com>
2022-04-16 14:39:43 -0500

    latest changes to accomodate transfer

--- src/api/hooks/transfer-fees.js
+++ src/api/hooks/transfer-fees.js
@@ -1,13 +1,14 @@
-import { computeOutputWrappedBTC } from 'zero-protocol/lib/badger';
+import { computeOutputBTC } from 'zero-protocol/lib/badger';
 import { ethers } from "ethers";
 
 function useTransferFees(){    
     async function getTransferOutput({ amount, token }) {
         const input = {
             asset: token,
-            amount: ethers.utils.parseUnits(amount, 18)
+            amount: ethers.utils.parseUnits(amount, 18),
+            type: 'transfer'
         }
-        let output = await computeOutputWrappedBTC(input);
+        let output = await computeOutputBTC(input);
         output = ethers.utils.formatEther(output);
         return output;
     }
--- src/ui/molecules/bridge.transfer/bridge.fee.js
+++ src/ui/molecules/bridge.transfer/bridge.fee.js
@@ -33,13 +33,13 @@ export const BridgeTransferFee = ({ amount, effect, tokenPrice, setToken, token 
       <>
         {amount > 0 && (
           <div className="self-center px-0 py-0 w-full">
-              <div className="w-full shadow-inner flex justify-between px-5 py-2 mt-5 text-white rounded-xl dark:bg-badger-gray-500 bg-gray-100">
+              <div className="w-full shadow-inner flex justify-between px-4 py-2 mt-5 text-white rounded-xl dark:bg-badger-gray-500 bg-gray-100">
                   <div className="flex items-center max-w-[100%]">
-                    <p className="text-[10px] text-gray-300 whitespace-nowrap">FEE</p>
+                    <p className="text-[10px] text-gray-300 whitespace-nowrap">AFTER FEES {'&'} GAS</p>
                   </div>
                   <div>
                       <span className={`${isFeeLoading && "animate-pulse"}`}>
-                          {fee || 0} BTC
+                          {fee || 0} {token}
                       </span>
                   </div>
               </div>
