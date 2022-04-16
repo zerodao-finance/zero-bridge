8d2cb106158fb01cd364a1c5c49d606534425c92
Nick K <nicholas.khami@gmail.com>
2022-04-16 14:08:09 -0500

    componentized the bridge fee

---
+++ src/ui/molecules/bridge.transfer/bridge.fee.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import useTransferFees from '../../../api/hooks/transfer-fees'
+
+export const BridgeTransferFee = ({ amount, effect, tokenPrice, setToken, token }) => {
+    // How to use 'TokenDropdown' component
+    // Create 'useState' variables and pass it to 'TokenDropdown' 
+    // (props are 'token' and 'setToken' where 'token' is a string of the token symbol)
+
+    // Additional Props: 'tokensRemoved' which is an array of tokens you do not want in dropdown
+    // Default Dropdown Items: ETH, WBTC, ibBTC, renBTC, USDC
+
+    // Getting Fees - START
+    const { getTransferOutput } = useTransferFees();
+    const [isFeeLoading, setIsFeeLoading] = useState(false);
+    const [fee, setFee] = useState();
+    useEffect(async () => {
+        if(amount > 0) {
+            setIsFeeLoading(true);
+            const output = await getTransferOutput({ amount, token });
+            setFee(output);
+            setIsFeeLoading(false);
+        }
+    }, [amount, token])
+    // Getting Fees - END
+
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: "currency",
+        currency: "USD"
+    })
+
+    return (
+      <>
+        {amount > 0 && (
+          <div className="self-center px-0 py-0 w-full">
+              <div className="w-full shadow-inner flex justify-between px-5 py-2 mt-5 text-white rounded-xl dark:bg-badger-gray-500 bg-gray-100">
+                  <div className="flex items-center max-w-[100%]">
+                    <p className="text-[10px] text-gray-300 whitespace-nowrap">FEE</p>
+                  </div>
+                  <div>
+                      <span className={`${isFeeLoading && "animate-pulse"}`}>
+                          {fee || 0} BTC
+                      </span>
+                  </div>
+              </div>
+              <div className=" xl:mr-5 italic tracking-wider w-full text-right text-[10px] text-badger-yellow-neon-400">
+                  ~ { tokenPrice && formatter.format(fee * ethers.utils.formatUnits(tokenPrice, 6)) }
+              </div> 
+          </div>
+        )}
+      </>
+    )
+}
--- src/ui/molecules/bridge.transfer/bridge.transfer.js
+++ src/ui/molecules/bridge.transfer/bridge.transfer.js
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import { BridgeTransferInput } from './bridge.transfer.input'
-import { BridgeTransferResult } from './bridge.transfer.result'
-import { BridgeTransferRatio } from './bridge.transfer.ratio'
+import { BridgeTransferFee } from './bridge.fee'
 import { BridgeTransferSubmit } from './bridge.transfer.submit'
-import { BridgeModuleToggle } from './bridge.module.toggle'
 import { BridgeLoadingSignature } from './bridge.loading.signature'
 import { BridgeLoadingGateway } from './bridge.loading.gateway'
 import { BridgeGatewayConfirmation } from '../bridge.gateway/bridge.gateway'
@@ -11,8 +9,6 @@ import { AiOutlineArrowDown } from 'react-icons/ai'
 import { useBridgeInput } from '../../../api/global/interfaces/interface.bridge.transfer'
 import { useCheckWalletConnected } from '../../../api/global/interfaces/interfaces.wallet'
 import BridgeTransferFrom from './bridge.transfer.from'
-import useTransferFees from '../../../api/hooks/transfer-fees'
-import { useEffect } from 'react'
 
 export const BridgeTransferModule = ({ mode }) => {
     const { 
@@ -23,21 +19,6 @@ export const BridgeTransferModule = ({ mode }) => {
         getTransferModuleToggleProps,
         getGatewayData,
     } = useBridgeInput();
-
-    // Getting Fees - START
-    const { getTransferOutput } = useTransferFees();
-    const { amount, token } = getTransferInputProps();
-    const [isFeeLoading, setIsFeeLoading] = useState(false);
-    const [fee, setFee] = useState();
-    useEffect(async () => {
-        if(amount > 0) {
-            setIsFeeLoading(true);
-            const output = await getTransferOutput({ amount, token });
-            setFee(output);
-            setIsFeeLoading(false);
-        }
-    }, [amount, token])
-    // Getting Fees - END
     
     const { 
         open
@@ -58,19 +39,8 @@ export const BridgeTransferModule = ({ mode }) => {
                                 <BridgeTransferInput {...getTransferInputProps()}/>
                             </div>
                         </div>
-                        
-                        {amount > 0 && (
-                            <div className="w-full shadow-inner flex justify-between px-5 py-2 mt-5 text-white rounded-xl dark:bg-badger-gray-500 bg-gray-100">
-                                <div>
-                                    <span>Fee</span>
-                                </div>
-                                <div>
-                                    <span className={`${isFeeLoading && "animate-pulse"}`}>
-                                        {fee || 0}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
+
+                        <BridgeTransferFee {...getTransferInputProps()}/>
                     </div>
                 </div>
                 
