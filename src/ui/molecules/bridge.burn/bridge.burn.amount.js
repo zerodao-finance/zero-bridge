698acf184805e027bb132949962d1ea51d201eb3
Nick K <nicholas.khami@gmail.com>
2022-04-19 15:57:03 -0500

    correct appearance

--- src/ui/molecules/bridge.burn/bridge.burn.amount.js
+++ src/ui/molecules/bridge.burn/bridge.burn.amount.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import { DefaultInput } from '../../atoms/inputs/input.default'
+import { ArrowDownIcon } from '@heroicons/react/solid'
 import { FaEthereum } from 'react-icons/fa'
 import { ethers } from 'ethers'
 import TokenDropdown from '../../atoms/dropdowns/dropdown.tokens'
+import { BridgeBurnTransferFee } from './bridge.burn.fee'
 
 export const BridgeBurnInput = ({ destination, amount, setToken, token, updateDestination, effect, tokenPrice }) => {
     var formatter = new Intl.NumberFormat('en-US', {
@@ -32,6 +34,23 @@ export const BridgeBurnInput = ({ destination, amount, setToken, token, updateDe
                 </div>
                 <div className=" xl:mr-5 italic tracking-wider w-full text-right text-[10px] text-badger-yellow-neon-400">
                     ~ { tokenPrice && formattedAmount() }
+                </div> 
+                {
+                    amount > 0 && (
+                        <>
+                            <div className="w-full grid justify-items-center">
+                                <ArrowDownIcon className="h-6 w-6 text-badger-yellow-400" />
+                            </div>
+                            <BridgeBurnTransferFee {...{amount: amount, token: token, tokenPrice: tokenPrice}}/>
+                        </>
+                    )
+                }
+
+                <div className="w-full py-1 mt-16 items-center justify-between gap-2 dark:bg-badger-gray-500 bg-gray-100 px-2 rounded-2xl">
+                    <div>
+                        <p className="text-[10px] text-gray-300 whitespace-nowrap mt-1">RECIPIENT BTC ADDRESS</p> 
+                    </div>
+                    <DefaultInput value={destination} onChange={updateDestination} type={ 'text' }/>
                 </div> 
             </div>
         
--- src/ui/molecules/bridge.burn/bridge.burn.js
+++ src/ui/molecules/bridge.burn/bridge.burn.js
@@ -5,7 +5,6 @@ import { BridgeBurnResult } from './bridge.burn.result'
 import { BridgeBurnInput } from './bridge.burn.amount'
 import { BridgeBurnSubmit } from './bridge.burn.submit'
 import { useBridgeBurnInput } from "../../../api/global/interfaces/interface.bridge.burn"
-import { BridgeBurnTransferFee } from "./bridge.burn.fee"
 
 export const BridgeBurnModule = () => {
     const { open } = useCheckWalletConnected()
@@ -29,11 +28,6 @@ export const BridgeBurnModule = () => {
                     <div className="animate-flip-in-hor-top [animation-delay:400ms] container h-max flex flex-row place-content-center max-w-[25rem] gap-3 md:gap-5 justify-around items-center px-1 md:px-8 z-10">
                         <div className="flex flex-col">
                             <BridgeBurnInput {...getBridgeBurnInputProps()}/>
-                        </div>
-                    </div>
-                    <div className={` animate-flip-in-hor-top [animation-delay:600ms] container h-max flex flex-col place-content-center max-w-[25rem] gap-3 md:gap-5 justify-around items-center px-1 md:px-8  pt-8 pb-4`}>
-                        <div className="flex flex-col w-full">
-                            <BridgeBurnTransferFee {...getBridgeBurnInputProps()}/>
                         </div>
                     </div>
                     <div className="animate-flip-in-hor-top [animation-delay:700ms] mt-4">
