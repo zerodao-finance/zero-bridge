35d4fe5fb374dad915e5b9774db9df0f5111b3fc
Nick K <nicholas.khami@gmail.com>
2022-04-23 13:32:26 -0500

    no autofill and cleaner style

--- src/ui/atoms/inputs/input.default.js
+++ src/ui/atoms/inputs/input.default.js
@@ -2,7 +2,6 @@ export const DefaultInput = ({ value, onChange, type, onClick, disabled, loading
 	return (
 		<div className="w-full text-right p-2">
 			<input
-				id="transfer-amount"
 				className={`${disabled ? 
 					`${loading && "animate-pulse"} dark:text-gray-300 form-input !outline-offset-0 !outline-1 text-right bg-transparent focus:ring-0 text-md font-medium text-gray-600 z-40 w-full rounded-xl` : 
 					`dark:text-white form-input !outline-offset-0 !outline-1 text-right bg-transparent border !border-gray-600 focus:!border-badger-yellow-neon-400 focus:!outline-badger-yellow-neon-400 dark:focus:!border-badger-yellow-neon-400 dark:!border-white focus:ring-0 text-md font-medium text-gray-600 z-40 w-full rounded-xl`
@@ -13,8 +12,7 @@ export const DefaultInput = ({ value, onChange, type, onClick, disabled, loading
 				onChange={onChange}
 				onClick={onClick}
 				disabled={disabled}
-				style={style, {maxWidth: maxW}}
-				placeholder="0"
+				style={{maxWidth: maxW}}
 			/>
 		</div>
 	);
