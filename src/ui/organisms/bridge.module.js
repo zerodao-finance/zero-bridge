10a51b0a4683714d236ed1e668168a61facfb60b
Nick K <nicholas.khami@gmail.com>
2022-05-07 12:12:57 -0500

    dropdown below button issue

--- src/ui/organisms/bridge.module.js
+++ src/ui/organisms/bridge.module.js
@@ -14,17 +14,17 @@ export const BridgeModule = ({ wallet, mode, toggleMode }) => {
   return !tcSigned ? (
     <Disclaimer />
   ) : (
-    <div className="h-full w-full pr-[2rem] pl-[2rem] pb-[2rem] flex flex-col container h-fit bg-white shadow-xl rounded-[8px] justify-center place-items-center gap-1 md:gap-3  first:gap-0 w-fit dark:bg-badger-black-500 text-badger-white-400 min-w-[370px]">
+    <div className="h-full w-full flex flex-col container h-fit bg-white shadow-xl rounded-lg justify-center place-items-center gap-1 md:gap-3  first:gap-0 w-fit dark:bg-badger-black-500 text-badger-white-400 min-w-[370px]">
       <div
-        className={`h-full w-[calc(100%+4rem)] overflow-hidden rounded-t-[8px] grid grid-cols-2 grid-flow-rows mb-8 bg-gray-200 dark:bg-badger-gray-400 align-center font-light tracking-wider text-sm text-center`}
+        className={`h-full w-full overflow-hidden rounded-t-lg grid grid-cols-2 grid-flow-rows mb-8 bg-gray-200 dark:bg-badger-gray-400 align-center font-light tracking-wider text-sm text-center`}
         style={{ maxHeight: "42px" }}
       >
         <Link to="/transfer">
           <div
-            className={`py-[10px] rounded-tl-[8px] cursor-pointer ${
+            className={`py-2.5 rounded-tl-lg cursor-pointer ${
               window.location.hash.includes("/transfer")
                 ? "transition ease-in-out duration-150 text-black border-b-2 border-badger-yellow-400 dark:text-badger-yellow-400 font-bold"
-                : "transition ease-in-out duration-150 text-black border-b-2 border-transparent hover:bg-badger-yellow-400/10 dark:text-[#8a8a8a] font-bold"
+                : "transition ease-in-out duration-150 text-black border-b-2 border-transparent hover:bg-badger-yellow-400/10 dark:text-badger-gray-600 font-bold"
             }`}
           >
             TRANSFER
@@ -32,10 +32,10 @@ export const BridgeModule = ({ wallet, mode, toggleMode }) => {
         </Link>
         <Link to="/release">
           <div
-            className={`py-[10px] rounded-tr-[8px] cursor-pointer ${
+            className={`py-2.5 rounded-tr-lg cursor-pointer ${
               window.location.hash.includes("/release")
                 ? "transition ease-in-out duration-150 text-black border-b-2 border-badger-yellow-400 dark:text-badger-yellow-400 font-bold"
-                : "transition ease-in-out duration-150 text-black border-b-2 border-transparent hover:bg-badger-yellow-400/10 dark:text-[#8a8a8a] font-bold"
+                : "transition ease-in-out duration-150 text-black border-b-2 border-transparent hover:bg-badger-yellow-400/10 dark:text-badger-gray-600 font-bold"
             }`}
           >
             RELEASE
@@ -43,17 +43,19 @@ export const BridgeModule = ({ wallet, mode, toggleMode }) => {
         </Link>
       </div>
 
-      {wallet ? (
-        <BridgeLoadingWallet />
-      ) : (
-        <Routes>
-          <Route
-            path="/transfer/*"
-            element={<BridgeTransferModule {...getTransferMode()} />}
-          />
-          <Route path="/release/*" element={<BridgeBurnModule />} />
-        </Routes>
-      )}
+      <div className="px-8 pb-8">
+        {wallet ? (
+          <BridgeLoadingWallet />
+        ) : (
+          <Routes>
+            <Route
+              path="/transfer/*"
+              element={<BridgeTransferModule {...getTransferMode()} />}
+            />
+            <Route path="/release/*" element={<BridgeBurnModule />} />
+          </Routes>
+        )}
+      </div>
     </div>
   );
 };
