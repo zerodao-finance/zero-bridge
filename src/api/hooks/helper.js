7ab835a2b877ceb34fe1043214a4eee99493ae07
Nick K <nicholas.khami@gmail.com>
2022-07-21 15:26:56 -0500

    should be right with inTx

--- src/api/hooks/helper.js
+++ src/api/hooks/helper.js
@@ -181,7 +181,7 @@ class SDKHelper {
     var forwarded = null;
 
     //production code
-    await transaction.eventEmitter.on("progress", (progress) => {
+    await transaction.inTx.on("progress", (progress) => {
       console.log("PROGRESS: ", progress);
       if (progress.status === "ready") {
         const { id, dispatch } = task.this.Notify.createTXCard(
