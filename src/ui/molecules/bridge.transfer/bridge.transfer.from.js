c4be02fdf0d550cb0ea1c35fb6fccd3ecd90bd46
yoyobojo <angellolazar@gmail.com>
2022-11-19 16:27:10 -0500

    vault balance css

--- src/ui/molecules/bridge.transfer/bridge.transfer.from.js
+++ src/ui/molecules/bridge.transfer/bridge.transfer.from.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { DefaultInput } from "../../atoms";
 import { formatUSDCPricedBTC } from "../../../api/utils/formatters";
 import PrimaryTokenDropdown from "../../atoms/dropdowns/dropdown.primary.tokens";
 import { CheckboxInput } from "../../atoms/inputs/input.checkbox";
 import { checkVaultAmount } from "../../../api/utils/sdk";
+import { Transition } from "@headlessui/react";
 
 function BridgeTransferFrom({
   amount,
@@ -42,13 +43,26 @@ function BridgeTransferFrom({
   return (
     <>
       <div className="self-center px-0 py-0 w-full">
-        {oneConfEnabled ? (
-          <div className=" xl:mr-5 tracking-wider pr-2 w-full flex justify-end text-xs text-zero-neon-green-500 mt-2">
-            <span>Vault Balance: {vaultBalance} renBTC</span>
-          </div>
-        ) : (
-          <div className="mt-6" />
-        )}
+        <div className="min-h-[16px]">
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-75"
+            enterFrom="transform opacity-0"
+            enterTo="transform opacity-100"
+            leave="transition duration-75"
+            leaveFrom="transform opacity-100"
+            leaveTo="transform opacity-0"
+            show={oneConfEnabled}
+          >
+            <div
+              className={`xl:mr-5 tracking-wider pr-2 w-full flex justify-end text-xs text-zero-neon-green-500 ${
+                Number(vaultBalance) === 0 ? "animate-pulse" : ""
+              }`}
+            >
+              <span>Vault Balance: {vaultBalance} renBTC</span>
+            </div>
+          </Transition>
+        </div>
         <div className="w-full flex items-center justify-between px-4 py-2 text-badger-white-400 rounded-xl bg-badger-gray-500">
           <div className="grid">
             <p className="text-xs text-gray-300 whitespace-nowrap">FROM</p>
