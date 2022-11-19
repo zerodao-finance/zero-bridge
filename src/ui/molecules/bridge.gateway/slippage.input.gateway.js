a2638b5ecf972cdcf26266f48c42d8c82c71fb1b
yoyobojo <angellolazar@gmail.com>
2022-11-19 16:12:02 -0500

    transition on settings

--- src/ui/molecules/bridge.gateway/slippage.input.gateway.js
+++ src/ui/molecules/bridge.gateway/slippage.input.gateway.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { CogIcon } from "@heroicons/react/outline";
 import OutsideClickHandler from "react-outside-click-handler";
+import { Transition } from "@headlessui/react";
 
 const evmRegex = /^0x[a-fA-F0-9]{40}$/;
 
@@ -84,81 +85,93 @@ export const SlippageInput = ({
   return (
     <OutsideClickHandler onOutsideClick={() => setOpenSetting(false)}>
       <div className="w-full flex justify-end">
-        <CogIcon
+        <button
           onClick={() => setOpenSetting(!openSetting)}
-          className="h-6 w-6 cursor-pointer"
-        />
-        <div
-          className={
-            "bg-badger-black-400 absolute z-20 mt-8 select-none p-4 rounded-lg text-badger-white grid " +
-            (openSetting ? "" : "hidden")
-          }
+          className="hover:text-neutral-300 transition duration-150"
         >
-          <span className="text-sm font-semibold text-badger-text-secondary">
-            Transaction Settings
-          </span>
-          <div>
-            <label htmlFor="slipTolerance" className="text-sm mt-2">
-              Slippage Tolerance
-            </label>
-            <div className="flex mt-1 text-sm">
-              <button
-                className="hover:bg-zero-green-500/40 bg-zero-green-500/90 rounded-lg p-2 font-bold text-badger-white-400 px-4"
-                onClick={() => autoSlippage()}
-              >
-                Auto
-              </button>
-              <input
-                type="number"
-                name="slipTolerance"
-                id="slipTolerance"
-                className={
-                  "block rounded-lg ml-2 text-right border-1 ring-1 text-badger-black-800 pr-7 font-semibold " +
-                  (valid
-                    ? "focus:border-badger-gray-200 focus:ring-badger-gray-200 ring-transparent"
-                    : "border-error-red-400 ring-error-red-400 focus:ring-error-red-400 focus:border-error-red-400")
-                }
-                placeholder="0.1"
-                value={slippage}
-                onChange={(e) => setSlippage(e.target.value)}
-              />
-              <div className="absolute pt-3 right-3 pr-3 flex items-center pointer-events-none text-badger-black-800 font-bold">
-                %
-              </div>
-            </div>
-            {type == "transfer" && (
-              <div className="mt-4">
-                <label htmlFor="slipTolerance" className="text-sm mt-2">
-                  Destination Address
-                </label>
-                <div className="flex mt-1 text-sm">
-                  <button
-                    className="hover:bg-zero-green-500/40 bg-zero-green-500/90 rounded-lg p-2 font-bold text-badger-white-400"
-                    onClick={() => setDestinationAddress(defaultAddress)}
-                  >
-                    Default
-                  </button>
-                  <input
-                    type="text"
-                    name="destinationAddress"
-                    id="destinationAddress"
-                    className={
-                      "block rounded-lg ml-2 border-1 ring-1 text-badger-black-800 pr-4 w-full font-semibold " +
-                      (addressValid
-                        ? "focus:border-badger-gray-200 focus:ring-badger-gray-200 ring-transparent"
-                        : "border-error-red-400 ring-error-red-400 focus:ring-error-red-400 focus:border-error-red-400")
-                    }
-                    placeholder="0.1"
-                    value={destinationAddress || ""}
-                    onChange={(e) => {
-                      setDestinationAddress(e.target.value);
-                    }}
-                  />
+          <CogIcon className="h-6 w-6" />
+        </button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+          show={openSetting}
+        >
+          <div
+            className={
+              "bg-badger-black-400 absolute z-20 mt-8 select-none p-4 rounded-lg text-badger-white grid "
+            }
+          >
+            <span className="text-sm font-semibold text-badger-text-secondary">
+              Transaction Settings
+            </span>
+            <div>
+              <label htmlFor="slipTolerance" className="text-sm mt-2">
+                Slippage Tolerance
+              </label>
+              <div className="flex mt-1 text-sm">
+                <button
+                  className="hover:bg-zero-green-500/40 bg-zero-green-500/90 rounded-lg p-2 font-bold text-badger-white-400 px-4"
+                  onClick={() => autoSlippage()}
+                >
+                  Auto
+                </button>
+                <input
+                  type="number"
+                  name="slipTolerance"
+                  id="slipTolerance"
+                  className={
+                    "block rounded-lg ml-2 text-right border-1 ring-1 text-badger-black-800 pr-7 font-semibold " +
+                    (valid
+                      ? "focus:border-badger-gray-200 focus:ring-badger-gray-200 ring-transparent"
+                      : "border-error-red-400 ring-error-red-400 focus:ring-error-red-400 focus:border-error-red-400")
+                  }
+                  placeholder="0.1"
+                  value={slippage}
+                  onChange={(e) => setSlippage(e.target.value)}
+                />
+                <div className="absolute pt-3 right-3 pr-3 flex items-center pointer-events-none text-badger-black-800 font-bold">
+                  %
                 </div>
               </div>
-            )}
+              {type == "transfer" && (
+                <div className="mt-4">
+                  <label htmlFor="slipTolerance" className="text-sm mt-2">
+                    Destination Address
+                  </label>
+                  <div className="flex mt-1 text-sm">
+                    <button
+                      className="hover:bg-zero-green-500/40 bg-zero-green-500/90 rounded-lg p-2 font-bold text-badger-white-400"
+                      onClick={() => setDestinationAddress(defaultAddress)}
+                    >
+                      Default
+                    </button>
+                    <input
+                      type="text"
+                      name="destinationAddress"
+                      id="destinationAddress"
+                      className={
+                        "block rounded-lg ml-2 border-1 ring-1 text-badger-black-800 pr-4 w-full font-semibold " +
+                        (addressValid
+                          ? "focus:border-badger-gray-200 focus:ring-badger-gray-200 ring-transparent"
+                          : "border-error-red-400 ring-error-red-400 focus:ring-error-red-400 focus:border-error-red-400")
+                      }
+                      placeholder="0.1"
+                      value={destinationAddress || ""}
+                      onChange={(e) => {
+                        setDestinationAddress(e.target.value);
+                      }}
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+        </Transition>
       </div>
     </OutsideClickHandler>
   );
